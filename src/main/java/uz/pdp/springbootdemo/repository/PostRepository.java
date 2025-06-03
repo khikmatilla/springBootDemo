14e3fd7eb20486ac64fb0ac93bc8446c44ffe473
hikmatilla <hikmatillahabibullayev1999@gmail.com>
2025-06-03 22:29:47 +0500

    mkhjuygio

--- src/main/java/uz/pdp/springbootdemo/repository/PostRepository.java
+++ src/main/java/uz/pdp/springbootdemo/repository/PostRepository.java
@@ -1,7 +1,10 @@
 package uz.pdp.springbootdemo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 import uz.pdp.springbootdemo.post.Post;
 
+@RepositoryRestResource(path = "maqolalar", collectionResourceRel = "maqolalar")
 public interface PostRepository extends JpaRepository<Post, Integer> {
 }
